eee0cfca7826b94ce5ef53161aef2cbdb1caf2b2
JeckaSh <JeckaShevelkov@gmail.com>
2022-08-10 21:56:59 +0300

    Add new monsters, icons and descriptions for items (#2)

--- lib/domain/model/deck/standard/enemy.dart
+++ lib/domain/model/deck/standard/enemy.dart
@@ -96,3 +96,280 @@ class ZombieWarrior extends Card with Door, Enemy {
   @override
   int get levels => 1;
 }
+
+class Lich extends Card with Door, Enemy {
+  @override
+  String get name => 'Lich';
+
+  @override
+  String? get asset => 'door/enemy/lich.png';
+
+  @override
+  int get level => 22;
+
+  @override
+  int get treasures => 3;
+
+  @override
+  int get levels => 2;
+}
+
+class Imp extends Card with Door, Enemy {
+  @override
+  String get name => 'Imp';
+
+  @override
+  String get description => 'A small demon';
+
+  @override
+  String? get asset => 'door/enemy/imp.png';
+
+  @override
+  int get level => 7;
+
+  @override
+  int get treasures => 1;
+
+  @override
+  int get levels => 1;
+}
+
+class BigImp extends Card with Door, Enemy {
+  @override
+  String get name => 'Big Imp';
+
+  @override
+  String get description => 'A big scary demon';
+
+  @override
+  String? get asset => 'door/enemy/big_imp.png';
+
+  @override
+  int get level => 15;
+
+  @override
+  int get treasures => 2;
+
+  @override
+  int get levels => 1;
+}
+
+class Cast extends Card with Door, Enemy {
+  @override
+  String get name => 'Cast';
+
+  @override
+  String get description => 'A scary cast';
+
+  @override
+  String? get asset => 'door/enemy/cast.png';
+
+  @override
+  int get level => 8;
+
+  @override
+  int get treasures => 1;
+
+  @override
+  int get levels => 1;
+}
+
+class Mummy extends Card with Door, Enemy {
+  @override
+  String get name => 'Mummy';
+
+  @override
+  String get description => 'A mummy from Egypt';
+
+  @override
+  String? get asset => 'door/enemy/mummy.png';
+
+  @override
+  int get level => 8;
+
+  @override
+  int get treasures => 1;
+
+  @override
+  int get levels => 1;
+}
+
+class WildTroll extends Card with Door, Enemy {
+  @override
+  String get name => 'Wild Troll';
+
+  @override
+  String get description => 'He is looks dangerous';
+
+  @override
+  String? get asset => 'door/enemy/wild_troll.png';
+
+  @override
+  int get level => 10;
+
+  @override
+  int get treasures => 2;
+
+  @override
+  int get levels => 1;
+}
+
+class Ogr extends Card with Door, Enemy {
+  @override
+  String get name => 'Ogr';
+
+  @override
+  String get description => 'HULK';
+
+  @override
+  String? get asset => 'door/enemy/ogr.png';
+
+  @override
+  int get level => 13;
+
+  @override
+  int get treasures => 2;
+
+  @override
+  int get levels => 1;
+}
+
+class StoneGolem extends Card with Door, Enemy {
+  @override
+  String get name => 'Stone Golem';
+
+  @override
+  String get description => 'STONE HULK';
+
+  @override
+  String? get asset => 'door/enemy/stone_golem.png';
+
+  @override
+  int get level => 15;
+
+  @override
+  int get treasures => 2;
+
+  @override
+  int get levels => 1;
+}
+
+class Gargoly extends Card with Door, Enemy {
+  @override
+  String get name => 'Gargoly';
+
+  @override
+  String get description => 'none';
+
+  @override
+  String? get asset => 'door/enemy/gargoly.png';
+
+  @override
+  int get level => 10;
+
+  @override
+  int get treasures => 1;
+
+  @override
+  int get levels => 1;
+}
+
+class WaterGirlMonster extends Card with Door, Enemy {
+  @override
+  String get name => 'Water Girl Monster';
+
+  @override
+  String get description => 'none';
+
+  @override
+  String? get asset => 'door/enemy/water_girl_monster.png';
+
+  @override
+  int get level => 8;
+
+  @override
+  int get treasures => 1;
+
+  @override
+  int get levels => 1;
+}
+
+class StrangeWolf extends Card with Door, Enemy {
+  @override
+  String get name => 'Strange Wolf';
+
+  @override
+  String get description => 'none';
+
+  @override
+  String? get asset => 'door/enemy/strange_wolf.png';
+
+  @override
+  int get level => 9;
+
+  @override
+  int get treasures => 1;
+
+  @override
+  int get levels => 1;
+}
+
+class AngryMage extends Card with Door, Enemy {
+  @override
+  String get name => 'Angry Mage';
+
+  @override
+  String get description => 'none';
+
+  @override
+  String? get asset => 'door/enemy/angry_mage.png';
+
+  @override
+  int get level => 11;
+
+  @override
+  int get treasures => 1;
+
+  @override
+  int get levels => 1;
+}
+
+class Necromant extends Card with Door, Enemy {
+  @override
+  String get name => 'Necromant';
+
+  @override
+  String get description => 'none';
+
+  @override
+  String? get asset => 'door/enemy/necromant.png';
+
+  @override
+  int get level => 13;
+
+  @override
+  int get treasures => 2;
+
+  @override
+  int get levels => 1;
+}
+
+class Dracula extends Card with Door, Enemy {
+  @override
+  String get name => 'Dracula';
+
+  @override
+  String get description => 'none';
+
+  @override
+  String? get asset => 'door/enemy/dracula.png';
+
+  @override
+  int get level => 17;
+
+  @override
+  int get treasures => 2;
+
+  @override
+  int get levels => 1;
+}
--- lib/domain/model/deck/standard/equipable.dart
+++ lib/domain/model/deck/standard/equipable.dart
@@ -6,6 +6,9 @@ class WoodenHelmet extends Card with Treasure, Equipable, Head {
 
   @override
   int get bonus => 1;
+
+  @override
+  String get description => 'Adds +1 power';
 }
 
 class WoodenArmor extends Card with Treasure, Equipable, Torso {
@@ -14,6 +17,9 @@ class WoodenArmor extends Card with Treasure, Equipable, Torso {
 
   @override
   int get bonus => 1;
+
+  @override
+  String get description => 'Adds +1 power';
 }
 
 class WoodenShoes extends Card with Treasure, Equipable, Shoes {
@@ -22,6 +28,9 @@ class WoodenShoes extends Card with Treasure, Equipable, Shoes {
 
   @override
   int get bonus => 1;
+
+  @override
+  String get description => 'Adds +1 power';
 }
 
 class IronSword extends Card with Treasure, Equipable, Hand {
@@ -30,4 +39,24 @@ class IronSword extends Card with Treasure, Equipable, Hand {
 
   @override
   int get bonus => 3;
+
+  @override
+  String get description => 'Adds +3 power';
+
+  @override
+  String? get asset => 'treasure/iron_sword.png';
+}
+
+class IronAxe extends Card with Treasure, Equipable, Hand {
+  @override
+  String get name => 'Iron Axe';
+
+  @override
+  int get bonus => 4;
+
+  @override
+  String get description => 'Adds +4 power';
+
+  @override
+  String? get asset => 'treasure/iron_axe.png';
 }
